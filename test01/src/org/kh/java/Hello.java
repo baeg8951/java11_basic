1d2dfdab77bb70a40e8d8ad362c924117400abcc
baeg8951 <baeg8951@naver.com>
2024-02-21 14:12:04 +0900

    java 11 insallation & variable & operation

---
+++ test01/src/org/kh/java/Hello.java
@@ -0,0 +1,15 @@
+package org.kh.java;
+
+public class Hello {
+	public static void main(String[] args) {  //실행을 위한 기본 main 메소드 = ctrl+/ = 한줄 각주
+		System.out.println("Hello~! \nJAVA~!");
+		/*
+		 자기 이름 소개하는 
+		 출력 명령
+		 결과 : My name is KiTae1004 = ctrl + shift +/ = 여러 줄 각주
+		 */
+		//sysout => ctrl=spacebar = 단축명령 자동 완성(Code hinting)
+		System.out.println("My name is KiTae1004");
+	}
+	
+}
